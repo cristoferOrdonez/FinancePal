e7b9088a945a889e76adabd8d4f0af3d829c07d4
farid <faridardila22@gmail.com>
2023-11-01 00:53:28 -0500

    Cambios en el listview, agregué la tabla de gastos

--- app/src/main/java/com/example/financepal/Gastos.java
+++ app/src/main/java/com/example/financepal/Gastos.java
@@ -2,13 +2,17 @@ package com.example.financepal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 public class Gastos extends AppCompatActivity {
-
+    String correoElectronicoS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gastos);
+        correoElectronicoS = getIntent().getStringExtra("correoElectronico");
     }
 }
