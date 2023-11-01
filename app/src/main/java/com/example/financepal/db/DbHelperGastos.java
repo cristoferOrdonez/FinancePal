aadcbc85ae4723a0628f45657cf478b9b4a145c2
farid <faridardila22@gmail.com>
2023-11-01 00:53:28 -0500

    Cambios en el listview, agregué la tabla de gastos

---
+++ app/src/main/java/com/example/financepal/db/DbHelperGastos.java
@@ -0,0 +1,35 @@
+package com.example.financepal.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DbHelperGastos extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NOMBRE = "gastos.db";
+    public static final String TABLE_GASTOS = "t_gastosu"
+
+    public DbHelperGastos(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NOMBRE, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " +TABLE_GASTOS +"(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "correo TEXT NOT NULL," +
+                "nombre TEXT NOT NULL," +
+                "monto BIGINT NOT NULL," +
+                "prioridad ENUM('Baja', 'Media Baja', 'Media', 'Media Alta', 'Alta') NOT NULL," +
+                "recurrencia TINYINT NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE "+TABLE_GASTOS);
+        onCreate(sqLiteDatabase);
+    }
+}
