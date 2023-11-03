82cc5aa450e5b811257b0c8cb185b0e96e7257de
farid <faridardila22@gmail.com>
2023-11-03 01:01:53 -0500

    se agregó la base de datos categoria de gastos, arreglo de botones xml

--- app/src/main/java/com/example/financepal/AgregarGasto.java
+++ app/src/main/java/com/example/financepal/AgregarGasto.java
@@ -2,13 +2,38 @@ package com.example.financepal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AgregarGasto extends AppCompatActivity {
+
+    String correoElectronicoS;
+    EditText nombre, monto, recurrencia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        correoElectronicoS = getIntent().getStringExtra("correoElectronico");
         setContentView(R.layout.activity_agregar_gasto);
+
+    }
+
+    public void cambiaraAtras(View view){
+        Intent miIntent = new Intent(this, Gastos.class);
+        miIntent.putExtra("correoElectronico", correoElectronicoS);
+        startActivity(miIntent);
+        finishAffinity();
+    }
+
+    public void guardarCambios(View view){
+        Intent miIntent = new Intent(this, Gastos.class);
+        miIntent.putExtra("correoElectronico", correoElectronicoS);
+
+
+
+        startActivity(miIntent);
+        finishAffinity();
     }
 }
