9df2155614ac6394cd86aed36e56e63adc5e6530
Diego Arévalo <darevaloa@unal.edu.co>
2023-11-21 22:40:40 -0500

    Balance estructura posible, metodo de metasmensualestotales implementado, logo en el inicio, lista_item_historico para el historico

--- app/src/main/java/com/example/financepal/Balance.java
+++ app/src/main/java/com/example/financepal/Balance.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.example.financepal.db.DbNombreMetas;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Balance extends AppCompatActivity {
 
     String correoElectronicoS;
     ImageView botonInicio, botonHistorico, botonMisDatos;
+
+    EditText espacioIngresosTotales, espacioGastoTotales, espacioMetasMensualesTotales, espacioBalance, espacioMayorGasto, espacioGastoMasRecurrente, espacioGastoMenorPrioridad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,39 @@ public class Balance extends AppCompatActivity {
 
         botonMisDatos = findViewById(R.id.botonMisDatosBALANCE);
         botonMisDatos.setOnClickListener(view -> cambiarAMisDatos(view));
+
+
+        espacioIngresosTotales=findViewById(R.id.espacioIngresosTotales);
+        espacioGastoTotales=findViewById(R.id.espacioGastosTotales);
+        espacioMetasMensualesTotales=findViewById(R.id.espacioMetasMensualesTotales);
+        espacioBalance=findViewById(R.id.espacioBalance);
+        espacioMayorGasto=findViewById(R.id.espacioMayorGasto);
+        espacioGastoMasRecurrente=findViewById(R.id.espacioGastoMasRecurrente);
+        espacioGastoMenorPrioridad=findViewById(R.id.espacioGastoMenorPrioridad);
+
+        espacioIngresosTotales.setInputType(InputType.TYPE_NULL);
+        espacioGastoTotales.setInputType(InputType.TYPE_NULL);
+        espacioMetasMensualesTotales.setInputType(InputType.TYPE_NULL);
+        espacioBalance.setInputType(InputType.TYPE_NULL);
+        espacioMayorGasto.setInputType(InputType.TYPE_NULL);
+        espacioGastoMasRecurrente.setInputType(InputType.TYPE_NULL);
+        espacioGastoMenorPrioridad.setInputType(InputType.TYPE_NULL);
+
+
+        NumberFormat col = NumberFormat.getCurrencyInstance(new Locale("es", "CO"));
+
+
+
+
+
+
+
+        DbNombreMetas dbNombreMetas =new DbNombreMetas(Balance.this);
+        Double montomensualmetas =dbNombreMetas.sumarMontosMensuales(correoElectronicoS);
+        espacioMetasMensualesTotales.setText((col.format(montomensualmetas))+" COP");
+        dbNombreMetas.close();
+
+
 
     }
 
