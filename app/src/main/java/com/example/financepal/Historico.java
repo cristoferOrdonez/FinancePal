b780fc94f8b53d47fa05fc55d505cedb96a3d94c
farid <faridardila22@gmail.com>
2023-11-24 22:54:40 -0500

    Agregue el listview del historico y el adapter funcional

--- app/src/main/java/com/example/financepal/Historico.java
+++ app/src/main/java/com/example/financepal/Historico.java
@@ -6,18 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.example.financepal.adaptadores.AdaptadorHistorico;
+import com.example.financepal.adaptadores.CustomAdapterGastos;
+import com.example.financepal.db.DbGastos;
+import com.example.financepal.db.DbHistorico;
+import com.example.financepal.entidades.EntidadHistorico;
+import com.example.financepal.entidades.UsuarioGastos;
+
+import java.util.List;
 
 public class Historico extends AppCompatActivity {
 
     String correoElectronicoS;
     ImageView botonInicio, botonBalance, botonMisDatos;
+    private DbHistorico db;
+    List<EntidadHistorico> lista;
+
+    ListView ListViewHistorico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historico);
-
+        db = new DbHistorico(this);
         correoElectronicoS = getIntent().getStringExtra("correoElectronico");
 
         botonInicio = findViewById(R.id.botonInicioHISTORICO);
@@ -28,6 +42,16 @@ public class Historico extends AppCompatActivity {
 
         botonMisDatos = findViewById(R.id.botonMisDatosHISTORICO);
         botonMisDatos.setOnClickListener(view -> cambiarAMisDatos(view));
+
+        ListViewHistorico = findViewById(R.id.listViewHistorico);
+        try {
+            lista=db.mostrarHistorico(correoElectronicoS);
+            AdaptadorHistorico adapter = new AdaptadorHistorico(this,lista);
+            ListViewHistorico.setAdapter(adapter);
+        }catch (Exception e){
+            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+        }
+
 
     }
 
